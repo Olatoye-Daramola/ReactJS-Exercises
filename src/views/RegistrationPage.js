84f5fb835bbe0f8b382876dc82c8dfba9b4c8695
Olatoye Daramola <daramolatoye@gmail.com>
2021-11-14 14:19:13 +0100

    ...

--- src/views/RegistrationPage.js
+++ src/views/RegistrationPage.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import InputField from "../components/beansApp/InputField";
 import Button from "../components/beansApp/Button";
-import '../beansApp.css'
+import Background from "../components/beansApp/Background";
+import '../css/register_login.css'
+
 
 const RegistrationPage = () => {
     return (
         <div className="container">
-            <div className="registration_page">
-                <img className="top_circle" src="../assets/background_circle.svg" alt="half_circle_background"/>
+            <div className="registration_page_container">
+                <Background/>
+                <div className="intro_container">
+                    <div className="intro_text">
+                        <h3>Welcome To Ewa</h3>
+                        <p>Where your cravings for beans are satisfied</p>
+                    </div>
+                </div>
+
+
                 <InputField label="Username" fieldType="text" placeholder="Enter your username"/>
                 <InputField label="First Name" fieldType="text" placeholder="Enter your First Name"/>
                 <InputField label="Last Name" fieldType="text" placeholder="Enter your Last Name"/>
                 <InputField label="Password" fieldType="password" placeholder="Enter your Password"/>
+
                 <Button className="registration_btn" buttonName="Register"/>
+
             </div>
         </div>
     );
